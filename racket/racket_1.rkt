db1d4ae0b5ccba458957cfd3cbfe485bb29a4429
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-08 19:48:04 -0700

    add is subset function

--- racket/racket_1.rkt
+++ racket/racket_1.rkt
@@ -31,6 +31,19 @@ Developer E-mail : neilmathias25@gmail.com
       -1
     ))
 
+;;
+; isSubset
+; determines whether list 1 is a subset of list 2
+
+(define (my-subset? lst1 lst2)
+  (if (empty? lst1)
+      #t
+      (if (contains (first lst1) lst2)
+          (my-subset? (rest lst1) lst2)
+           #f
+        )
+    ))
+
 '("Tests for length of list")
 (define list_1 (list 1 (list 2 3) 3 5 3))
 (list_len list_1)
@@ -43,3 +56,11 @@ Developer E-mail : neilmathias25@gmail.com
 (index-of 3 empty_list)
 (index-of 5 list_1)
 (index-of 16 '(1 4 9))
+
+'("Tests for subset function")
+(my-subset? '() '())
+(my-subset? '() '(1 2))
+(my-subset? '(2 1 1) '(1 2))
+(my-subset? '(2 1 3) '(1 2))
+(my-subset? '(a d (3 1)) '(1 a (3 d)))
+(my-subset? '((1) (2 2)) '((3 3 3) a (2 2) (1)))
