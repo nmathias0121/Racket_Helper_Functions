15f7913d086a8829b5f16e780662b0e70f769f8c
Neil Mathias <neilmathias25@gmail.com>
2023-12-18 15:57:18 -0800

    handle empty list

--- haskell/list_operations.hs
+++ haskell/list_operations.hs
@@ -12,20 +12,21 @@ listSize :: [a] -> Int
 listSize lst = length lst
 
 -- first lst -> returns first element of the list (lst !! 1)
---lst is not empty
 first :: [a] -> a
-first lst = head (lst)
+first lst | listSize lst == 0 = error "list is empty"
+          | otherwise = head (lst)
 
 -- fifth lst -> returns fifth element of the list (lst !! 4)
---lst has at least five elements
 fifth :: [a] -> a
-fifth lst = head (take 1 (drop 4 lst))
+fifth lst | listSize lst < 5 = error "there is no 5th element"
+          | otherwise = head (take 1 (drop 4 lst))
 
 -- get n lst -> returns nth element of the list (lst !! n)
---lst has at least n elements
 -- example input : get 5 [1,2,3,4,5,6,7] => 5
 get :: Int -> [a] -> a
-get n lst = head (drop (n-1) lst)
+get n lst | listSize lst < n = error "there are not enough elements in this list"
+          | otherwise = head (drop (n-1) lst)
+
 
 -- Exception if b or h non-positive
 triangle_area :: (Fractional a, Eq a, Ord a) => a -> a -> a
