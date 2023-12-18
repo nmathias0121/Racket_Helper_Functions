e5342a3a8716ae596b56e9f4dd639ff986008fca
Neil Mathias <neilmathias25@gmail.com>
2023-12-18 15:51:03 -0800

    add area and heron area of triangle and calculate size of list

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Haskell: <br />
  -> list_operations.rkt contains functions to find length of list, index of an element in a list, subset, pairs, determine duplicates, find elements with multiple occurences, sum of all elements in a list, maximum value <br />
  -> propositions.rkt contains functions to determine if element is a digit or letter, find if a variable name is eligible, determine truth-false values of propositions and solve them<br />
 - Haskell<br />
- -> list_operations.hs contains function to get nth element of the list
+ -> list_operations.hs contains function to get nth element of the list, calculate area and heron area of triangle
 
 
 ## To Display Number of Lines in a Repository:
--- haskell/list_operations.hs
+++ haskell/list_operations.hs
@@ -6,6 +6,10 @@ Developer Name : Neil Mathias
 Developer E-mail : neilmathias25@gmail.com
 -}
 
+-- Calculate the size of the list
+-- list can contain element of only one type
+listSize :: [a] -> Int
+listSize lst = length lst
 
 -- first lst -> returns first element of the list (lst !! 1)
 --lst is not empty
@@ -22,3 +26,19 @@ fifth lst = head (take 1 (drop 4 lst))
 -- example input : get 5 [1,2,3,4,5,6,7] => 5
 get :: Int -> [a] -> a
 get n lst = head (drop (n-1) lst)
+
+-- Exception if b or h non-positive
+triangle_area :: (Fractional a, Eq a, Ord a) => a -> a -> a
+triangle_area b h | b <= 0  = error "triangle_area: b <= 0"
+                  | h <= 0 = error "triangle_area: h <= 0"
+                  | otherwise = (b * h) / 2
+
+-- Exception if a, b or c non-positive
+heron_area :: (Fractional a, Eq a, Floating a, Ord a) => a -> a -> a -> a
+heron_area a b c | a <= 0  = error "heron_area: a must be positive"
+                 | b <= 0 = error "heron_area: b must be positive"
+                 | c <= 0 = error "heron_area: c must be positive"
+                 | value < 0 = error "heron_area: not a triangle"
+                 | otherwise = sqrt value / 4
+                        where
+                            value = (a + b + c) * (c - (a - b)) * (c + (a - b)) * (a + (b - c))
